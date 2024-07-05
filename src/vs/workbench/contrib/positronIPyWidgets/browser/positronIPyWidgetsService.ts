5fb2a70da383761bcece58c4cd3103bbc7184a01
seem <mwlorgat@gmail.com>
2024-07-05 11:10:38 +0200

    refactor checkin

--- src/vs/workbench/contrib/positronIPyWidgets/browser/positronIPyWidgetsService.ts
+++ src/vs/workbench/contrib/positronIPyWidgets/browser/positronIPyWidgetsService.ts
@@ -16,7 +16,6 @@ import { isEqual } from 'vs/base/common/resources';
 import { ILogService } from 'vs/platform/log/common/log';
 import { asWebviewUri } from 'vs/workbench/contrib/webview/common/webview';
 import { IExtensionService } from 'vs/workbench/services/extensions/common/extensions';
-import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { IRuntimeCommMessage, IWidgetCommMessage } from './types';
 import { RuntimeClientState } from 'vs/workbench/services/languageRuntime/common/languageRuntimeClientInstance';
 
@@ -29,27 +28,27 @@ export class PositronIPyWidgetsService extends Disposable implements IPositronIP
 	/** The emitter for the onDidCreatePlot event */
 	private readonly _onDidCreatePlot = new Emitter<WidgetPlotClient>();
 
-	/** Creates the Positron plots service instance */
 	constructor(
 		@IRuntimeSessionService private _runtimeSessionService: IRuntimeSessionService,
 		@INotebookEditorService private _notebookEditorService: INotebookEditorService,
 		@ILogService private _logService: ILogService,
-		@IInstantiationService private _instantiationService: IInstantiationService,
+		@IExtensionService private _extensionService: IExtensionService,
 	) {
 		super();
 
+		// Start ipywidgets instances for existing sessions.
 		this._runtimeSessionService.activeSessions.forEach(session => {
-			this.attachRuntime(session);
+			this.startIPyWidgetsInstance(session);
 		});
 
-		// Register for language runtime service startups
+		// Start ipywidgets instances for new sessions.
 		// TODO: Should we listen to onWillStartSession instead?
 		this._register(this._runtimeSessionService.onDidStartRuntime((session) => {
-			this.attachRuntime(session);
+			this.startIPyWidgetsInstance(session);
 		}));
 	}
 
-	private attachRuntime(session: ILanguageRuntimeSession) {
+	private startIPyWidgetsInstance(session: ILanguageRuntimeSession) {
 		// We currently only handle notebook sessions.
 		// TODO: Support console sessions too.
 		if (session.metadata.sessionMode !== LanguageRuntimeSessionMode.Notebook) {
@@ -65,35 +64,42 @@ export class PositronIPyWidgetsService extends Disposable implements IPositronIP
 			return;
 		}
 
-		// We found a matching notebook editor, start a PositronIPyWidgetsInstance.
+		// We found a matching notebook editor, start an ipywidgets instance.
 		this._logService.debug(`Found an existing notebook editor for session '${session.sessionId}, starting ipywidgets instance`);
-		const iPyWidgetsInstance = this._instantiationService.createInstance(
-			IPyWidgetsInstance,
+		const iPyWidgetsInstance = new IPyWidgetsInstance(
 			session,
-			notebookEditor);
-		this._iPyWidgetsInstancesBySessionId.set(session.sessionId, iPyWidgetsInstance);
+			notebookEditor,
+			this._extensionService,
+			this._logService,
+		);
+		this._iPyWidgetsInstancesBySessionId.set(
+			session.sessionId,
+			iPyWidgetsInstance
+		);
 
 		const disposableStore = new DisposableStore();
 
 		// TODO: Does this ever fire?
 		// Dispose the instance when the model changes.
 		disposableStore.add(notebookEditor.onDidChangeModel((e) => {
-			if (!isEqual(session.metadata.notebookUri, e?.uri)) {
-				this._logService.debug(`Editor model changed for session '${session.sessionId}, disposing ipywidgets instance`);
-				this._iPyWidgetsInstancesBySessionId.delete(session.sessionId);
-				iPyWidgetsInstance.dispose();
-				disposableStore.dispose();
+			if (isEqual(session.metadata.notebookUri, e?.uri)) {
+				return;
 			}
+			this._logService.debug(`Editor model changed for session '${session.sessionId}, disposing ipywidgets instance`);
+			this._iPyWidgetsInstancesBySessionId.delete(session.sessionId);
+			iPyWidgetsInstance.dispose();
+			disposableStore.dispose();
 		}));
 
 		// Clean up when the notebook editor is removed.
 		disposableStore.add(this._notebookEditorService.onDidRemoveNotebookEditor((e) => {
-			if (e === notebookEditor) {
-				this._logService.debug(`Notebook editor removed for session '${session.sessionId}, disposing ipywidgets instance`);
-				this._iPyWidgetsInstancesBySessionId.delete(session.sessionId);
-				iPyWidgetsInstance.dispose();
-				disposableStore.dispose();
+			if (e !== notebookEditor) {
+				return;
 			}
+			this._logService.debug(`Notebook editor removed for session '${session.sessionId}, disposing ipywidgets instance`);
+			this._iPyWidgetsInstancesBySessionId.delete(session.sessionId);
+			iPyWidgetsInstance.dispose();
+			disposableStore.dispose();
 		}));
 	}
 
@@ -111,53 +117,58 @@ class IPyWidgetsInstance extends Disposable {
 	private readonly _clients = new Map<string, WidgetClientInstance>();
 
 	/**
-	 * Constructor.
-	 *
 	 * @param _session The language runtime session.
+	 * @param _editor The notebook editor.
+	 * @param _extensionService The extension service.
+	 * @param _logService The log service.
 	 */
 	constructor(
-		private _session: ILanguageRuntimeSession,
-		private _editor: INotebookEditor,
-		@IExtensionService private _extensionService: IExtensionService,
+		private readonly _session: ILanguageRuntimeSession,
+		private readonly _editor: INotebookEditor,
+		private readonly _extensionService: IExtensionService,
+		_logService: ILogService,
 	) {
-		// Call the base class's constructor.
 		super();
 
-		// Attach to the runtime.
-		this.attachRuntime();
-	}
+		const frontend = new WidgetsFrontend(_editor, _logService);
 
-	/**
-	 * Attaches to a runtime.
-	 */
-	private attachRuntime() {
-		this._register(this._editor.onDidReceiveMessage(async (event) => {
-			// TODO: Add types...
-			const message = event.message as any;
-			switch (message.type) {
-				case 'comm_msg':
-					this.handleCommMsg(message);
-					break;
-				default:
-					console.warn('Unhandled message:', message);
-					break;
+		// Handle the creation of new IPyWidget client instances.
+		this._register(this._session.onDidCreateClientInstance(({ client, message }) => {
+			if (client.getClientType() !== RuntimeClientType.IPyWidget) {
+				return;
 			}
+
+			// Notify the widget frontend that a new client instance was created.
+			// TODO: Move this into WidgetClientInstance constructor?
+			frontend.createClient(client.getClientId(), message.data, message.metadata);
+
+			// TODO: Can there be a race condition here somehow?
+			// TODO: Maybe we should move this above the postMessage.
+			const instance = new WidgetClientInstance(client, this._editor);
+			this._clients.set(client.getClientId(), instance);
 		}));
 
-		this._register(this._session.onDidCreateClientInstance((event) => {
-			if (event.client.getClientType() === RuntimeClientType.IPyWidget) {
-				this._editor.postMessage({
-					type: 'comm_open',
-					comm_id: event.message.comm_id,
-					target_name: event.message.target_name,
-					content: { data: event.message.data },
-					metadata: event.message.metadata,
-				});
-
-				// TODO: Can there be a race condition here somehow?
-				// TODO: Maybe we should move this above the postMessage.
-				const instance = new WidgetClientInstance(event.client, this._editor);
-				this._clients.set(event.client.getClientId(), instance);
+		// Handle messages from widget frontends (i.e. the ipywidgets notebook preload script).
+		this._register(frontend.onDidReceiveMessage(async (message) => {
+			console.log('SEND comm_msg:', message.comm_id, message.content);
+			const content = message.content;
+			const client = this._clients.get(message.comm_id);
+			if (!client) {
+				throw new Error(`Client not found for comm_id: ${message.comm_id}`);
+			}
+			// TODO: Maybe it's better to separate these in the preload? Is that possible?
+			if (['request_states', 'update'].includes(content?.method)) {
+				const output = await client.performRpc(content, 5000);
+				console.log('RECV comm_msg:', output);
+				const reply: IRuntimeCommMessage = {
+					type: 'comm_msg',
+					comm_id: message.comm_id,
+					parent_header: { msg_id: message.msg_id },
+					content: { data: output }
+				};
+				this._editor.postMessage(reply);
+			} else {
+				client.sendMessage(content);
 			}
 		}));
 
@@ -171,29 +182,6 @@ class IPyWidgetsInstance extends Disposable {
 				}));
 			this._editor.postMessage({ type: 'append_stylesheet', href: styleUri.toString() });
 		});
-	}
-
-	private async handleCommMsg(message: IWidgetCommMessage) {
-		const content = message.content;
-		console.log('SEND comm_msg:', message.comm_id, content);
-		const client = this._clients.get(message.comm_id);
-		if (!client) {
-			throw new Error(`Client not found for comm_id: ${message.comm_id}`);
-		}
-		// TODO: Maybe it's better to separate these in the preload? Is that possible?
-		if (['request_states', 'update'].includes(content?.method)) {
-			const output = await client.performRpc(content, 5000);
-			console.log('RECV comm_msg:', output);
-			const reply: IRuntimeCommMessage = {
-				type: 'comm_msg',
-				comm_id: message.comm_id,
-				parent_header: { msg_id: message.msg_id },
-				content: { data: output }
-			};
-			this._editor.postMessage(reply);
-		} else {
-			client.sendMessage(content);
-		}
 	}
 
 }
@@ -308,5 +296,46 @@ class PositronWidgetComm extends Disposable {
 	 */
 	get clientId(): string {
 		return this.instance.getClientId();
+	}
+}
+
+class WidgetsFrontend extends Disposable {
+	private readonly _messageEmitter = new Emitter<IWidgetCommMessage>();
+
+	onDidReceiveMessage = this._messageEmitter.event;
+
+	constructor(
+		private readonly _editor: INotebookEditor,
+		private readonly _logService: ILogService,
+	) {
+		super();
+
+		this._register(this._editor.onDidReceiveMessage(async (event) => {
+			const message = event.message as IWidgetCommMessage;
+			switch (message.type) {
+				case 'comm_msg':
+					this._messageEmitter.fire(message);
+					break;
+				default:
+					this._logService.warn(`Unhandled message for notebook:`, this._editor.textModel?.uri);
+					break;
+			}
+		}));
+
+		// TODO: Should this class forward messages to the frontend? Or should its owner do that?
+		//       The parent class could be an "adapter"?
+		//       Maybe the WidgetClientInstance should actually notifiy the frontend on its own creation?
+
+	}
+
+	// TODO: Should type be a param?
+	createClient(id: string, params: any, metadata: any) {
+		this._editor.postMessage({
+			type: 'comm_open',
+			comm_id: id,
+			target_name: RuntimeClientType.IPyWidget,
+			content: { data: params },
+			metadata,
+		});
 	}
 }
